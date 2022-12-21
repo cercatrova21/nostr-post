9d51b658e0daa0abed6e653c637c847231894511
Mike Dilger <mike@ezicheq.com>
2022-12-22 10:54:36 +1300

    Method to post from command line

---
+++ src/bin/import_private_key.rs
@@ -0,0 +1,18 @@
+
+use nostr_proto::{Event, EventKind, PreEvent, PrivateKey, Unixtime};
+use zeroize::Zeroize;
+
+// Turn a hex private key into an encrypted private key
+fn main() {
+    // Put your private key here, but
+    // DO NOT COMMIT THIS!
+    let hex_private_key = "FIXME";
+
+    let mut password = rpassword::prompt_password("Password: ").unwrap();
+    let private_key = PrivateKey::try_from_hex_string(hex_private_key)
+        .expect("Could not import private key");
+    let encrypted_private_key = private_key.export_encrypted(&password)
+        .expect("Could not export encrypted private key");
+    println!("Encrypted private key: {}", encrypted_private_key);
+    password.zeroize();
+}
