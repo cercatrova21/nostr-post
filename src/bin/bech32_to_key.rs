af0d8794c8e86c899af303b7a55672d4c2f4cdc3
Mike Dilger <mike@mikedilger.com>
2022-12-23 17:42:09 +1300

    Conversion tools

---
+++ src/bin/bech32_to_key.rs
@@ -0,0 +1,16 @@
+
+use nostr_proto::{PublicKey, PrivateKey};
+
+fn main() {
+    let bech32 = rpassword::prompt_password("bech32: ").unwrap();
+
+    if let Ok(key) = PublicKey::try_from_bech32_string(&bech32) {
+        println!("Public Key: {}", key.as_hex_string());
+    }
+    else if let Ok(mut key) = PrivateKey::try_from_bech32_string(&bech32) {
+        println!("Private Key: {}", key.as_hex_string());
+    }
+    else {
+        println!("Invalid.");
+    }
+}
---
+++ src/bin/post_metadata.rs
@@ -0,0 +1,94 @@
+
+use nostr_proto::{ClientMessage, Event, EventKind, Id, Metadata, PreEvent, PrivateKey, PublicKey, Tag, Unixtime, Url};
+use tungstenite::protocol::Message;
+use zeroize::Zeroize;
+
+fn main() {
+    let encrypted_private_key = "b1l/OWdYnR4fmbKYAqOJNO+efo2o4LeRhySKIyRDVIYBcYQ0jxO43IqbcZVDonTLD3KR/Tm/d34PqhzlhBnupg==";
+
+    let private_key = {
+        let mut password = rpassword::prompt_password("Password: ").unwrap();
+        let private_key = PrivateKey::import_encrypted(encrypted_private_key, &password)
+            .expect("Could not import private key hex string");
+        password.zeroize();
+        private_key
+    };
+
+    let public_key = private_key.public_key();
+
+    let metadata = Metadata {
+        name: Some("Michael Dilger".to_string()),
+        about: Some("Author of Gossip client: https://github.com/mikedilger/gossip".to_string()),
+        picture: Some("https://avatars.githubusercontent.com/u/1669069".to_string()),
+        nip05: Some("_@mikedilger.com".to_string()),
+    };
+
+    let pre_event = PreEvent {
+        pubkey: public_key,
+        created_at: Unixtime::now().unwrap(),
+        kind: EventKind::Metadata,
+        tags: vec![],
+        content: serde_json::to_string(&metadata).unwrap(),
+        ots: None
+    };
+
+    let event = Event::new(pre_event, private_key).expect("Could not create event");
+
+    let client_message = ClientMessage::Event(Box::new(event));
+
+    let client_message_string = serde_json::to_string(&client_message)
+        .expect("Could not serialize client message");
+
+    println!("{}", client_message_string);
+
+    let relay_urls = [
+        "wss://nostr.mikedilger.com",
+        "wss://nostr-pub.wellorder.net",
+        "wss://nostr-relay.wlvs.space",
+        "wss://nostr.oxtr.dev",
+        "wss://relay.nostr.info",
+        "wss://relay.damus.io"
+
+        // HANGING "wss://relayer.fiatjaf.com",
+        //"wss://nostr.onsats.org",
+    ];
+
+    for relay in relay_urls.iter() {
+        let relay = *relay;
+
+        let uri: http::Uri = relay.parse::<http::Uri>()
+            .expect("Could not parse url");
+        let authority = uri.authority().
+            expect("Has no hostname").as_str();
+        let host = authority
+            .find('@')
+            .map(|idx| authority.split_at(idx + 1).1)
+            .unwrap_or_else(|| authority);
+        if host.is_empty() {
+            panic!("URL has empty hostname");
+        }
+
+        let key: [u8; 16] = rand::random();
+
+        let request = http::request::Request::builder()
+            .method("GET")
+            .header("Host", host)
+            .header("Connection", "Upgrade")
+            .header("Upgrade", "websocket")
+            .header("Sec-WebSocket-Version", "13")
+            .header("Sec-WebSocket-Key", base64::encode(key))
+            .uri(uri)
+            .body(())
+            .expect("Could not build request");
+
+        println!("Sending to {}", relay);
+
+        let (mut websocket, _response) = tungstenite::connect(request)
+            .expect("Could not connect to relay");
+
+        websocket.write_message(Message::Text(client_message_string.clone()))
+            .expect("Could not send message to relay");
+    }
+
+    println!("Done.");
+}
